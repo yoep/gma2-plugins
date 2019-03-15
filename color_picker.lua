092a145d3d3b8adbdf20c040394ec2bc762cc660
yoep <yoeri.peeters10@telenet.be>
2019-03-15 20:03:04 +0100

    Added groups, blocks and wings executor buttons with command actions

--- color_picker.lua
+++ color_picker.lua
@@ -72,105 +72,112 @@ function create_exec_buttons()
 
     log("Creating color picker exec buttons...");
 
-    create_sequence(sequence, "ZOOM", get_executor_index(_G.executor_zoom_index));
+    create_sequence(sequence, "ZOOM", get_full_executor_index(_G.executor_zoom_index));
     create_cue(sequence, 1, "ON", create_cue_cmd_on("1." .. effect_index .. "." .. _G.zoom_effect_line_index));
     create_cue(sequence, 2, "OFF", create_cue_cmd_off("1." .. effect_index .. "." .. _G.zoom_effect_line_index));
 
     sequence = sequence + 1;
-    create_sequence(sequence, "DIM", get_executor_index(_G.executor_dim_index));
+    create_sequence(sequence, "DIM", get_full_executor_index(_G.executor_dim_index));
     create_cue(sequence, 1, "ON", create_cue_cmd_on("1." .. effect_index .. "." .. _G.dim_effect_line_index));
     create_cue(sequence, 2, "OFF", create_cue_cmd_off("1." .. effect_index .. "." .. _G.dim_effect_line_index));
 
     sequence = sequence + 1;
-    create_sequence(sequence, "GOBO", get_executor_index(_G.executor_gobo_index));
+    create_sequence(sequence, "GOBO", get_full_executor_index(_G.executor_gobo_index));
     create_cue(sequence, 1, "ON", create_cue_cmd_on("1." .. effect_index .. ".6 Thru 1." .. effect_index .. ".7"));
     create_cue(sequence, 2, "OFF", create_cue_cmd_off("1." .. effect_index .. ".6 Thru 1." .. effect_index .. ".7"));
 
     sequence = sequence + 1;
-    create_sequence(sequence, "SHUTTER", get_executor_index(_G.executor_shutter_index));
+    create_sequence(sequence, "SHUTTER", get_full_executor_index(_G.executor_shutter_index));
     create_cue(sequence, 1, "ON", create_cue_cmd_on("1." .. effect_index .. "." .. _G.shutter_effect_line_index));
     create_cue(sequence, 2, "OFF", create_cue_cmd_off("1." .. effect_index .. "." .. _G.shutter_effect_line_index));
 
     sequence = sequence + 1;
-    create_sequence(sequence, "PRESETS", get_executor_index(_G.executor_presets_index), get_color_blue());
-    create_cue(sequence, 1, "CHASE 2",  string.format("GoTo Executor %s Cue %i", get_executor_index(_G.executor_form_index), 1));
-    create_cue(sequence, 2, "CHASE 3",  string.format("GoTo Executor %s Cue %i", get_executor_index(_G.executor_form_index), 1));
+    create_sequence(sequence, "PRESETS", get_full_executor_index(_G.executor_presets_index), get_color_blue(), true, true);
+    create_preset_cues(sequence);
 
     sequence = sequence + 1;
-    create_sequence(sequence, "COLOR 1", get_executor_index(_G.executor_color1_index));
+    create_sequence(sequence, "COLOR 1", get_full_executor_index(_G.executor_color1_index), nil, true);
     create_color_cues(sequence, 16);
 
     sequence = sequence + 1;
-    create_sequence(sequence, "COLOR 2", get_executor_index(_G.executor_color2_index));
+    create_sequence(sequence, "COLOR 2", get_full_executor_index(_G.executor_color2_index), nil, true);
     create_color_cues(sequence, 17);
 
     sequence = sequence + 1;
-    create_sequence(sequence, "COLOR", get_executor_index(_G.executor_color_index));
+    create_sequence(sequence, "COLOR", get_full_executor_index(_G.executor_color_index));
     create_cue(sequence, 1, "REL", string.format("Assign Effect 1.%i.1 Thru 1.%i.3 /mode=rel;", _G.effect_index, _G.effect_index));
     create_cue(sequence, 2, "ABS", string.format("Assign Effect 1.%i.1 Thru 1.%i.3 /mode=abs;", _G.effect_index, _G.effect_index));
 
     sequence = sequence + 1;
-    create_sequence(sequence, "GOBO 1", get_executor_index(_G.executor_gobo1_index));
+    create_sequence(sequence, "GOBO 1", get_full_executor_index(_G.executor_gobo1_index), nil, true);
     create_gobo_cues(sequence, 29);
 
     sequence = sequence + 1
-    create_sequence(sequence, "GOBO 2", get_executor_index(_G.executor_gobo2_index));
+    create_sequence(sequence, "GOBO 2", get_full_executor_index(_G.executor_gobo2_index), nil, true);
     create_gobo_cues(sequence, 30);
 
     sequence = sequence + 1;
-    create_sequence(sequence, "FORM", get_executor_index(_G.executor_form_index));
+    create_sequence(sequence, "FORM", get_full_executor_index(_G.executor_form_index), nil, true);
     create_form_cues(sequence);
 
     sequence = sequence + 1;
-    create_sequence(sequence, "GROUPS", get_executor_index(_G.executor_groups_index));
+    create_sequence(sequence, "GROUPS", get_full_executor_index(_G.executor_groups_index), nil, true);
+    create_group_cues(sequence);
 
     sequence = sequence + 1;
-    create_sequence(sequence, "BLOCKS", get_executor_index(_G.executor_blocks_index));
+    create_sequence(sequence, "BLOCKS", get_full_executor_index(_G.executor_blocks_index), nil, true);
+    create_block_cues(sequence);
 
     sequence = sequence + 1;
-    create_sequence(sequence, "WINGS", get_executor_index(_G.executor_wings_index));
+    create_sequence(sequence, "WINGS", get_full_executor_index(_G.executor_wings_index), nil, true);
+    create_wing_cues(sequence);
 
     sequence = sequence + 1;
-    create_sequence(sequence, "DIR", get_executor_index(_G.executor_dir_index));
+    create_sequence(sequence, "DIR", get_full_executor_index(_G.executor_dir_index), nil, true);
+    create_dir_cues(sequence);
 
     sequence = sequence + 1;
-    create_sequence(sequence, "BEAM", get_executor_index(_G.executor_beam_index), get_color_red());
+    create_sequence(sequence, "BEAM", get_full_executor_index(_G.executor_beam_index), get_color_red());
     create_cue(sequence, 1, "ON", create_cue_cmd_group_on(_G.beam_color_var, _G.beam_group_index));
     create_cue(sequence, 2, "OFF", create_cue_cmd_group_off(_G.beam_color_var, _G.beam_group_index));
 
     sequence = sequence + 1;
-    create_sequence(sequence, "SPOT", get_executor_index(_G.executor_spot_index), get_color_red());
+    create_sequence(sequence, "SPOT", get_full_executor_index(_G.executor_spot_index), get_color_red());
     create_cue(sequence, 1, "ON", create_cue_cmd_group_on(_G.spot_color_var, _G.spot_group_index));
     create_cue(sequence, 2, "OFF", create_cue_cmd_group_off(_G.spot_color_var, _G.spot_group_index));
 
     sequence = sequence + 1;
-    create_sequence(sequence, "WASH 1", get_executor_index(_G.executor_wash1_index), get_color_red());
+    create_sequence(sequence, "WASH 1", get_full_executor_index(_G.executor_wash1_index), get_color_red());
     create_cue(sequence, 1, "ON", create_cue_cmd_group_on(_G.wash1_color_var, _G.wash1_group_index));
     create_cue(sequence, 2, "OFF", create_cue_cmd_group_off(_G.wash1_color_var, _G.wash1_group_index));
 
     sequence = sequence + 1;
-    create_sequence(sequence, "WASH 2", get_executor_index(_G.executor_wash2_index), get_color_red());
+    create_sequence(sequence, "WASH 2", get_full_executor_index(_G.executor_wash2_index), get_color_red());
     create_cue(sequence, 1, "ON", create_cue_cmd_group_on(_G.wash2_color_var, _G.wash2_group_index));
     create_cue(sequence, 2, "OFF", create_cue_cmd_group_off(_G.wash2_color_var, _G.wash2_group_index));
 
     sequence = sequence + 1;
-    create_sequence(sequence, "LED", get_executor_index(_G.executor_led_index), get_color_red());
+    create_sequence(sequence, "LED", get_full_executor_index(_G.executor_led_index), get_color_red());
     create_cue(sequence, 1, "ON", create_cue_cmd_group_on(_G.led_color_var, _G.led_group_index));
     create_cue(sequence, 2, "OFF", create_cue_cmd_group_off(_G.led_color_var, _G.led_group_index));
 
     log("Done creating color picker exec buttons");
 end
 
-function get_executor_index(indexOffset)
-    return _G.exec_button_page .. "." .. indexOffset;
+function get_full_executor_index(executorIndex)
+    return _G.exec_button_page .. "." .. executorIndex;
 end
 
 --- Create/overwrite sequence
 ---@param seqIndex number The sequence index
 ---@param name string The name of the sequence
----@param executor number The executor index to link the sequence to
+---@param executor string The executor index to link the sequence to
 ---@param color string The color options for the sequence
-function create_sequence(seqIndex, name, executor, color)
+---@param isGoTo boolean Is the sequence executor a GoTo button
+---@param isRelease boolean Is the executor released after activation
+function create_sequence(seqIndex, name, executor, color, isGoTo, isRelease)
+    local executorOptions = "";
+
     log("Creating sequence " .. name .. " at index " .. seqIndex);
     gma.cmd(string.format("Store Sequence %i Cue 1 /nc", seqIndex));
     gma.cmd(string.format("Assign Sequence %i /name=\"%s\"", seqIndex, name));
@@ -179,18 +186,27 @@ function create_sequence(seqIndex, name, executor, color)
     if color ~= nil then
         gma.cmd(string.format("Appearance Sequence %i %s", seqIndex, color))
     end
+    if isRelease ~= nil then
+        executorOptions = "/cm=release";
+    end
+    if isGoTo ~= nil then
+        gma.cmd(string.format("Assign GoTo Executor %s %s", executor, executorOptions));
+    end
 end
 
-function create_cue(seqIndex, cueIndex, name, cmd)
+---@param sequence number The sequence index to create the cue in
+---@param cueIndex number The cue index in the sequence
+---@param name string The name of the cue
+function create_cue(sequence, cueIndex, name, cmd)
     local cueOptions = "";
 
-    gma.cmd(string.format("Store Sequence %i Cue %i /nc", seqIndex, cueIndex));
+    gma.cmd(string.format("Store Sequence %i Cue %i /nc", sequence, cueIndex));
 
     if cmd ~= nil then
         cueOptions = string.format(" /cmd=\"%s\"", cmd);
     end
 
-    gma.cmd(string.format('Assign Sequence %i Cue %i /name="%s" %s', seqIndex, cueIndex, name, cueOptions));
+    gma.cmd(string.format('Assign Sequence %i Cue %i /name="%s" %s', sequence, cueIndex, name, cueOptions));
 end
 
 function create_cue_cmd_on(effectIndex)
@@ -240,6 +256,22 @@ function create_cue_cmd_group_off(varName)
     return cmd;
 end
 
+function create_cue_cmd_form(cueIndex)
+    return string.format("GoTo Executor %s Cue %i; ", get_full_executor_index(_G.executor_form_index), cueIndex);
+end
+
+function create_cue_cmd_group(cueIndex)
+    return string.format("GoTo Executor %s Cue %i; ", get_full_executor_index(_G.executor_groups_index), cueIndex);
+end
+
+function create_cue_cmd_blocks(cueIndex)
+    return string.format("GoTo Executor %s Cue %i; ", get_full_executor_index(_G.executor_blocks_index), cueIndex);
+end
+
+function create_cue_cmd_executor_off(executorIndex)
+    return string.format("Off Executor %s; ", get_full_executor_index(executorIndex));
+end
+
 function create_color_cues(sequence, presetIndex)
     create_cue(sequence, 1, "WHITE", string.format("Copy Preset 4.1  At 4.%i /m", presetIndex))
     create_cue(sequence, 2, "RED", string.format("Copy Preset 4.2  At 4.%i /m", presetIndex))
@@ -268,6 +300,17 @@ function create_gobo_cues(sequence, presetIndex)
     create_cue(sequence, 9, "LIMBO", string.format("Copy Preset 3.9 At 3.%i /m", presetIndex))
 end
 
+function create_preset_cues(sequence)
+    create_cue(sequence, 1, "CHASE 2", create_cue_cmd_form(1) .. create_cue_cmd_group(1));
+    create_cue(sequence, 2, "CHASE 3", create_cue_cmd_form(1) .. create_cue_cmd_group(2));
+    create_cue(sequence, 3, "SIN 2", create_cue_cmd_form(2) .. create_cue_cmd_group(2));
+    create_cue(sequence, 4, "SIN 3", create_cue_cmd_form(2) .. create_cue_cmd_group(4));
+    create_cue(sequence, 5, "RAMP 2", create_cue_cmd_form(4) .. create_cue_cmd_group(1));
+    create_cue(sequence, 6, "RAMP 3", create_cue_cmd_form(4) .. create_cue_cmd_group(2));
+    create_cue(sequence, 7, "FULL", create_cue_cmd_form(5) .. create_cue_cmd_group(1));
+    create_cue(sequence, 8, "FULL EVEN/ODD", create_cue_cmd_form(5) .. create_cue_cmd_group(5));
+end
+
 function create_form_cues(sequence)
     create_cue(sequence, 1, "CHASE", string.format("Assign Form 5 At Effect %i; Assign Effect %i /phase=0..360 /width=100", _G.effect_index, _G.effect_index));
     create_cue(sequence, 2, "SIN", string.format("Assign Form 8 At Effect %i; Assign Effect %i /phase=0..360 /width=100", _G.effect_index, _G.effect_index));
@@ -278,6 +321,40 @@ function create_form_cues(sequence)
     create_cue(sequence, 7, "C SIN MY PWM", string.format("Assign Form 8 At Effect 1.%i.1; Assign Effect 1.%i.1 /phase=0..360 /width=100; Assign Form 4 At Effect 1.%i.1 Thru 1.%i.3 - 1.%i.1; Assign Effect 1.%i.2 /phase=90 /width=50; Assign Effect 1.%i.3 /phase=-90 /width=50", _G.effect_index, _G.effect_index, _G.effect_index, _G.effect_index, _G.effect_index, _G.effect_index, _G.effect_index));
     create_cue(sequence, 8, "M SIN CY PWM", string.format("Assign Form 8 At Effect 1.%i.2; Assign Effect 1.%i.2 /phase=0..360 /width=100; Assign Form 4 At Effect 1.%i.1 Thru 1.%i.3 - 1.%i.2; Assign Effect 1.%i.1 /phase=90 /width=50; Assign Effect 1.%i.3 /phase=-90 /width=50", _G.effect_index, _G.effect_index, _G.effect_index, _G.effect_index, _G.effect_index, _G.effect_index, _G.effect_index));
     create_cue(sequence, 9, "Y SIN CM PWM", string.format("Assign Form 8 At Effect 1.%i.3; Assign Effect 1.%i.3 /phase=0..360 /width=100; Assign Form 4 At Effect 1.%i.1 Thru 1.%i.3 - 1.%i.3; Assign Effect 1.%i.1 /phase=90 /width=50; Assign Effect 1.%i.2 /phase=-90 /width=50", _G.effect_index, _G.effect_index, _G.effect_index, _G.effect_index, _G.effect_index, _G.effect_index, _G.effect_index));
+end
+
+function create_group_cues(sequence)
+    create_cue(sequence, 0, "OFF", string.format("Assign Effect %i /groups=0 /rate=1; %s", effect_index, create_cue_cmd_executor_off(_G.executor_groups_index)));
+    create_cue(sequence, 1, "G2", string.format("Assign Effect %i /groups=2 /rate=0.5", effect_index));
+    create_cue(sequence, 2, "G3", string.format("Assign Effect %i /groups=3 /rate=0.33", effect_index));
+    create_cue(sequence, 3, "G4", string.format("Assign Effect %i /groups=4 /rate=0.25", effect_index));
+    create_cue(sequence, 4, "G6", string.format("Assign Effect %i /groups=6 /rate=0.16", effect_index));
+    create_cue(sequence, 5, "G8", string.format("Assign Effect %i /groups=8 /rate=0.125", effect_index));
+end
+
+function create_block_cues(sequence)
+    create_cue(sequence, 0, "OFF", string.format("Assign Effect %i /blocks=0; %s", effect_index, create_cue_cmd_executor_off(_G.executor_blocks_index)));
+    create_cue(sequence, 1, "B2", string.format("Assign Effect %i /blocks=2", effect_index));
+    create_cue(sequence, 2, "B3", string.format("Assign Effect %i /blocks=3", effect_index));
+    create_cue(sequence, 3, "B4", string.format("Assign Effect %i /blocks=4", effect_index));
+    create_cue(sequence, 4, "B6", string.format("Assign Effect %i /blocks=6", effect_index));
+    create_cue(sequence, 5, "B8", string.format("Assign Effect %i /blocks=8", effect_index));
+end
+
+function create_wing_cues(sequence)
+    create_cue(sequence, 0, "OFF", string.format("Assign Effect %i /wings=0; %s", effect_index, create_cue_cmd_executor_off(_G.executor_wings_index)));
+    create_cue(sequence, 1, "W2", string.format("Assign Effect %i /wings=2", effect_index));
+    create_cue(sequence, 2, "W3", string.format("Assign Effect %i /wings=3", effect_index));
+    create_cue(sequence, 3, "W4", string.format("Assign Effect %i /wings=4", effect_index));
+    create_cue(sequence, 4, "W6", string.format("Assign Effect %i /wings=6", effect_index));
+    create_cue(sequence, 5, "W8", string.format("Assign Effect %i /wings=8", effect_index));
+end
+
+function create_dir_cues(sequence)
+    create_cue(sequence, 1, "<", string.format("Assign Effect %i /dir=<", effect_index));
+    create_cue(sequence, 2, ">", string.format("Assign Effect %i /dir=>", effect_index));
+    create_cue(sequence, 3, "< BOUNCE", string.format("Assign Effect %i /dir=<bounce", effect_index));
+    create_cue(sequence, 4, "> BOUNCE", string.format("Assign Effect %i /dir=>bounce", effect_index));
 end
 
 function get_color_blue()
